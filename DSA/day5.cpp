ba4d3bfc9b311f80f41106e813f2f8b3dbec4f20
Aamna Anura <aamnaanura445@gmail.com>
2025-10-05 22:09:23 +0530

    Day5 1 question done.

---
+++ DSA/day5.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+// 1. --------------  return the maximum number of consecutive 1s in the array. ---------------
+class Solution
+{
+public:
+    int findMaxConsecutiveOnes(vector<int> &nums)
+    {
+        int n = nums.size();
+        int count = 0;
+        int max = 0;
+        for (int i = 0; i < n; i++)
+        {
+            if (nums[i] == 1)
+            {
+                count++;
+                if (count > max)
+                    max = count;
+            }
+            else
+            {
+                count = 0;
+            }
+        }
+        return max;
+    }
+};
